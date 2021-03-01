a28acf04782763369ccb2682efcdadf058a3c9f4
Samuel-Robles <robleshs@vt.edu>
2021-03-01 11:42:37 -0500

    intial commit

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    public float mouseSensitivityX = 250f;
+    public float mouseSensitivityY = 250f;
+    public float walkSpeed = 8f;
+    public float jumpForce = 220;
+
+    Transform cameraT;
+    float verticalLookRotation;
+
+    Vector3 moveAmount;
+    Vector3 smoothMoveVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraT = Camera.main.transform;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivityX);
+        verticalLookRotation += Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivityY;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation,-60f,60f);
+        cameraT.transform.localEulerAngles = Vector3.left * verticalLookRotation;
+        Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        Vector3 targetMoveAmount = moveDir * walkSpeed;
+        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, .15f);
+
+        if (Input.GetButtonDown("Jump")){
+            GetComponent<Rigidbody>().AddForce(transform.up * jumpForce);
+        }
+    }
+    void FixedUpdate(){
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+    }
+}
