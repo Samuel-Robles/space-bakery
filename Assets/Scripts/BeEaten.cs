5e677fb03415f7160e785fad9f48f33e5bfd3ea7
ccroft27 <53943290+ccroft27@users.noreply.github.com>
2021-03-03 11:17:45 -0500

    added audio

--- Assets/Scripts/BeEaten.cs
+++ Assets/Scripts/BeEaten.cs
@@ -5,25 +5,46 @@ using UnityEngine.EventSystems;
 
 public class BeEaten : MonoBehaviour
 {
+    private AudioSource audioSource;
+    private AudioClip clip;
+    private float volume=1f;
+    private bool spaceshipExists = true;
+    private bool twoaliensExists = true;
+    private bool alienExists = true;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && spaceshipExists)
         {
             print("E key was pressed");
             GameObject objectToDisappear = GameObject.Find("SpaceshipCake");
             objectToDisappear.GetComponent<Renderer>().enabled = false;
+            objectToDisappear = GameObject.Find("E");
+            objectToDisappear.GetComponent<Renderer>().enabled = false;
+            audioSource = GetComponent<AudioSource>();
+            audioSource.Play();
+            spaceshipExists = false;
         }
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R) && twoaliensExists)
         {
             print("R key was pressed");
             GameObject objectToDisappear = GameObject.Find("TwoAliensCake");
             objectToDisappear.GetComponent<Renderer>().enabled = false;
+            objectToDisappear = GameObject.Find("R");
+            objectToDisappear.GetComponent<Renderer>().enabled = false;
+            audioSource.PlayOneShot(clip, volume);
+            audioSource.Play();
+            twoaliensExists = false;
         }
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.T) && alienExists)
         {
             print("R key was pressed");
             GameObject objectToDisappear = GameObject.Find("aliencupcake");
             objectToDisappear.GetComponent<Renderer>().enabled = false;
+            objectToDisappear = GameObject.Find("T");
+            objectToDisappear.GetComponent<Renderer>().enabled = false;
+            audioSource.PlayOneShot(clip, volume);
+            audioSource.Play();
+            alienExists = false;
         }
     }
 }
