0ae6143053ac00552e4e461aa52889add73c9be1
ccroft27 <53943290+ccroft27@users.noreply.github.com>
2021-03-02 22:43:52 -0500

    cakes now disapear on respective key press

---
+++ Assets/Scripts/BeEaten.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BeEaten : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            print("E key was pressed");
+            GameObject objectToDisappear = GameObject.Find("SpaceshipCake");
+            objectToDisappear.GetComponent<Renderer>().enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            print("R key was pressed");
+            GameObject objectToDisappear = GameObject.Find("TwoAliensCake");
+            objectToDisappear.GetComponent<Renderer>().enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            print("R key was pressed");
+            GameObject objectToDisappear = GameObject.Find("aliencupcake");
+            objectToDisappear.GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
