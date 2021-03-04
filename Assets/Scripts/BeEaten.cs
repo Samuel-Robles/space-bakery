74d09167a4a241cc02c3c1bb9a5a13efe2325ee7
ccroft27 <53943290+ccroft27@users.noreply.github.com>
2021-03-03 20:32:48 -0500

    added paintings, and sign

--- Assets/Scripts/BeEaten.cs
+++ Assets/Scripts/BeEaten.cs
@@ -6,16 +6,17 @@ using UnityEngine.EventSystems;
 public class BeEaten : MonoBehaviour
 {
     private AudioSource audioSource;
-    private AudioClip clip;
-    private float volume=1f;
+    //private AudioClip clip;
+    //private float volume=1f;
     private bool spaceshipExists = true;
     private bool twoaliensExists = true;
     private bool alienExists = true;
+    private bool moonExists = true;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && spaceshipExists)
         {
-            print("E key was pressed");
+            //print("E key was pressed");
             GameObject objectToDisappear = GameObject.Find("SpaceshipCake");
             objectToDisappear.GetComponent<Renderer>().enabled = false;
             objectToDisappear = GameObject.Find("E");
@@ -26,25 +27,33 @@ public class BeEaten : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.R) && twoaliensExists)
         {
-            print("R key was pressed");
+            //print("R key was pressed");
             GameObject objectToDisappear = GameObject.Find("TwoAliensCake");
             objectToDisappear.GetComponent<Renderer>().enabled = false;
             objectToDisappear = GameObject.Find("R");
             objectToDisappear.GetComponent<Renderer>().enabled = false;
-            audioSource.PlayOneShot(clip, volume);
             audioSource.Play();
             twoaliensExists = false;
         }
         if (Input.GetKeyDown(KeyCode.T) && alienExists)
         {
-            print("R key was pressed");
+            //print("R key was pressed");
             GameObject objectToDisappear = GameObject.Find("aliencupcake");
             objectToDisappear.GetComponent<Renderer>().enabled = false;
             objectToDisappear = GameObject.Find("T");
             objectToDisappear.GetComponent<Renderer>().enabled = false;
-            audioSource.PlayOneShot(clip, volume);
             audioSource.Play();
             alienExists = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Y) && moonExists)
+        {
+            //print("Y key was pressed");
+            GameObject objectToDisappear = GameObject.Find("moonCake");
+            objectToDisappear.GetComponent<Renderer>().enabled = false;
+            objectToDisappear = GameObject.Find("Y");
+            objectToDisappear.GetComponent<Renderer>().enabled = false;
+            audioSource.Play();
+            moonExists = false;
         }
     }
 }
